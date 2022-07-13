6e20c5f18cfe258447bf9d91d238e2aae2879586
Stas Gavrilov <89335018+StasGavrilov@users.noreply.github.com>
2022-07-13 13:00:11 +0300

    5.7

---
+++ src/pages/Exmaple2Page.tsx
@@ -0,0 +1,14 @@
+import { Example2 } from '../components/examples/example2/Example2';
+
+export default function Example2Page() {
+    return (
+        <div>
+            <h1>Hello YouTube - AgGrid Example!</h1>
+            <Example2
+                onMoney={(money) => {
+                    alert(money);
+                }}
+            />
+        </div>
+    );
+}
