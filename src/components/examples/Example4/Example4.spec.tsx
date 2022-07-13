b9656b5602c8c51ad15f895fe71dce3675451106
Stas Gavrilov <89335018+StasGavrilov@users.noreply.github.com>
2022-07-13 16:47:58 +0300

    6

--- src/components/examples/Example4/Example4.spec.tsx
+++ src/components/examples/Example4/Example4.spec.tsx
@@ -9,9 +9,7 @@ createMock(MyDrawer).mockImplementation(() => <div>mocked: drawer</div>)
 describe('Example4', () => {
   it('renders MyDrawer', () => {
     render(<Example4 />)
-    expect(
-      screen.queryByText('Hello Drawer Component!')
-    ).not.toBeInTheDocument()
+    expect(screen.queryByText('Hello Drawer Component!')).not.toBeInTheDocument()
     expect(screen.getByText('mocked: drawer')).toBeInTheDocument()
   })
 })
